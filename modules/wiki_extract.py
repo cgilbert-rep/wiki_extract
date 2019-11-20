6866311bdf6070781d053af8e080c35d00919d05
cgilbert-rep <cgilb93@gmail.com>
2019-11-20 23:08:49 +0100

    error in all dump

--- modules/wiki_extract.py
+++ modules/wiki_extract.py
@@ -8,33 +8,34 @@ import time
 
 
 def run(argv=None):
-    # List of files to download
-    # # Get the dump url
-    dump_url = get_wikidump_url()
-    # # Get the list of file of the dump, sorted by ascending size
-    list_downloads_wikidump = sorted(
-        get_list_downloads_wikidump(
-            dump_url=dump_url),
-        key=lambda x: x[1],
-        reverse=False
-    )
-    # print(list_downloads_wikidump)
-    # Download files
-    print("Downloading dump")
-    start_time = time.time()
-    temp_folder = 'temp_files/'
-    for file in list_downloads_wikidump:
-        get_file_from_url(
-            url=file[0],
-            target_folder=temp_folder
-        )
-    print("Dump downloaded in %s s" % (time.time() - start_time))
+    # # List of files to download
+    # # # Get the dump url
+    # dump_url = get_wikidump_url()
+    # # # Get the list of file of the dump, sorted by ascending size
+    # list_downloads_wikidump = sorted(
+    #     get_list_downloads_wikidump(
+    #         dump_url=dump_url),
+    #     key=lambda x: x[1],
+    #     reverse=False
+    # )
+    # # print(list_downloads_wikidump)
+    # # Download files
+    # print("Downloading dump")
+    # start_time = time.time()
+    # temp_folder = 'temp_files/'
+    # for file in list_downloads_wikidump:
+    #     get_file_from_url(
+    #         url=file[0],
+    #         target_folder=temp_folder
+    #     )
+    # print("Dump downloaded in %s s" % (time.time() - start_time))
     # Preprocess files
     print("Preprocessing files")
     start_time = time.time()
     list_preprocess_files = [
         os.path.join(temp_folder, file)
         for file in os.listdir(temp_folder) if file.endswith(".bz2")]
+    print(list_preprocess_files)
     split_articles_partial = partial(
         split_articles,
         folder_output="output_files")
